1e13f7fb936d65a3472074fe4f1ca8084551e40e
David James McCorrie <djmccorrie@gmail.com>
2020-06-09 00:47:59 +0100

    PIR on interupt

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,30 @@
 #include <IRremote.h>
 
 const uint8_t RECV_PIN = 2;
+const uint8_t PIR_PIN = 3;
 const uint8_t buzzer = 5;
 
 struct LED3 chest_1 = {13, A0, 11};
 struct LED3 chest_2 = {A1, A3, A2};
 struct LED3 chest_3 = {4, A4, A5};
 struct LED3 chest_4 = {8, 7, 6};
-struct LED3 eyes = {12, 10, 9};
+struct LED3 eyes = {9, 10, 12};
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+bool motion = false;
+
+void motion_change(){
+  motion = !motion;
+
+  if(motion) {
+    write_led(chest_1, true, true, true);
+  }
+  else{
+    write_led(chest_1, false, false, false);
+  }
+}
 
 void setup() {
   setup_led(chest_1);
@@ -26,13 +39,17 @@ void setup() {
 
   irrecv.enableIRIn();
   irrecv.blink13(false);
+
+  pinMode(PIR_PIN, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(PIR_PIN), motion_change, CHANGE);
 }
 
 void loop() {
-  if (irrecv.decode(&results)){
-    /*if (results.value != 0xFFFFFFFF){
+/*  if (irrecv.decode(&results)){
+    if (results.value != 0xFFFFFFFF){
        Serial.println(results.value, HEX);
-    }*/
+    }
 
     if (results.value == IRC_DIY1){
       play_tune_aha_take_on_me(buzzer);
@@ -40,9 +57,19 @@ void loop() {
 
     irrecv.resume();
   }
+*/
   //cycle_led(chest_1);
   //cycle_led(chest_2);
   //cycle_led(chest_3);
   //cycle_led(chest_4);
-  cycle_led_pwm(eyes);
+  //cycle_led_pwm(eyes);
+
+  /*
+  if(digitalRead(PIR_PIN) == true){
+    motion_detected();
+  }
+  else {
+    motion_stopped();
+  }
+  */
 }
